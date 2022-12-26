1bc93c6746c4091a951be5c51235c6b7e1c53f20
Ștefan Tănăsescu <stefantanasescu91@gmail.com>
2022-12-26 14:54:59 +0200

    testing paths

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("navbar-items").onmousemove = e => {
+    for(const b of document.getElementsByClassName("navbutton")) {
+      const rect = b.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
