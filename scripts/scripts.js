54138112e6d2454a105ac89d6e919ba1040ea924
Ștefan Tănăsescu <stefantanasescu91@gmail.com>
2022-12-27 21:39:44 +0200

    basics of layout done+ some text formatting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,16 @@
 document.getElementById("navbar-items").onmousemove = e => {
-    for(const b of document.getElementsByClassName("navbutton")) {
-      const rect = b.getBoundingClientRect(),
+    for (const b of document.getElementsByClassName("navbutton")) {
+        const rect = b.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
-  
-      // card.style.setProperty("--mouse-x", `${x}px`);
-      // card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
 
-function testing(){
-  console.log("wow");
+        // card.style.setProperty("--mouse-x", `${x}px`);
+        // card.style.setProperty("--mouse-y", `${y}px`);
+    }
+    ;
+}
+
+function testing() {
+    console.log("wow");
 }
 
