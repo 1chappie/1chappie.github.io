0ee09d7370aede1f983bf2a5e8e67fdcf7b7a343
Ștefan Tănăsescu <stefantanasescu91@gmail.com>
2022-12-28 22:05:45 +0200

    implemented project page, fixed relative paths

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,6 @@
 function injectNavbar(activeElement) {
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "../assets/navbar.html", true);
+    xhr.open("GET", "/assets/navbar.html", true);
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let oldelem = document.querySelector("div#nb");
